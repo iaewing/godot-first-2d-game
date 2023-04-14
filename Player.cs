08fa8a3e099e488462af82da6acfa1f2a414e148
Ian <iewing085@gmail.com>
2023-04-14 14:38:33 -0400

    Add collision to the player

--- Player.cs
+++ Player.cs
@@ -2,15 +2,33 @@ using Godot;
 
 public partial class Player : Area2D
 {
+    [Signal]
+    public delegate void HitEventHandler();
+
     [Export]
     public int Speed = 400;
 
     public Vector2 ScreenSize;
+
+    private void OnBodyEntered(PhysicsBody2D body)
+    {
+        Hide();
+        EmitSignal(SignalName.Hit);
+        GetNode<CollisionShape2D>("CollisionShape2D").SetDeferred(CollisionShape2D.PropertyName.Disabled, true);
+    }
+
+    public void Start(Vector2 position)
+    {
+        Position = position;
+        Show();
+        GetNode<CollisionShape2D>("CollisionShape2D").Disabled = false;
+    }
+
     // Called when the node enters the scene tree for the first time.
     public override void _Ready()
     {
         ScreenSize = GetViewportRect().Size;
-		Hide();
+        Hide();
     }
 
     // Called every frame. 'delta' is the elapsed time since the previous frame.
